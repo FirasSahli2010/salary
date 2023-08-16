6ccd8a93ec1a67a3c500b64867678be79a4bbb94
Firas <feras.f.sahli@gmail.com>
2023-08-16 04:50:32 +0200

    Update packages

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Yectep\PhpSpreadsheetBundle\PhpSpreadsheetBundle::class => ['all' => true],
 ];
