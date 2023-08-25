8c8f4ea839a9fb2fa4d3fda8626dca0422162b58
Firas <feras.f.sahli@gmail.com>
2023-08-25 09:22:21 +0200

    write the files according to given parameters

--- src/Service/SalarySheetGeneratorService.php
+++ src/Service/SalarySheetGeneratorService.php
@@ -2,90 +2,119 @@
 namespace App\Service;
 
 // use App\Entity\Expense;
+use IntlDateFormatter;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Style\Alignment;
-use PhpOffice\PhpSpreadsheet\Style\Border;
-use PhpOffice\PhpSpreadsheet\Style\Color;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Yectep\PhpSpreadsheetBundle\Factory;
 
 class SalarySheetGeneratorService {
-    
-    private int $currentIndex = 1; // To be used when adding multiple monthes 
-    private ?string $reportPath = null;  // The path to the folder where the report will be generated, this now not used but it can be added
     private string $fileName;
     private SalaryHandlerService $salaryHandlerService;
-
-    private $month = 0;
-    private $year = 0;
+    private Factory $excelBundle;
+    private Spreadsheet $spreadsheet;
+    private int $currentIndex = 1;
+    private string $reportBasePath;
+    private ?string $reportPath = null;
     public function  __construct(
-        SalaryHandlerService $salaryHandlerService
-    ) 
-    {
-       $this->salaryHandlerService = $salaryHandlerService;
+        Factory $excelBundle,
+        ParameterBagInterface $parameterBag,
+        SalaryHandlerService $salaryHandlerService,
+    ) {
+        $this->salaryHandlerService = $salaryHandlerService;
+        $this->excelBundle = $excelBundle;
+        $this->spreadsheet = $excelBundle->createSpreadsheet();
+        $this->spreadsheet->setActiveSheetIndex(0);
+    }
+    private function writeHeader() {
+        $this
+            ->spreadsheet
+            ->getActiveSheet()
+            ->setCellValue("A$this->currentIndex", "Month")
+            ->setCellValue("B$this->currentIndex", "Salary paid on")
+            ->setCellValue("C$this->currentIndex", "Commission paid on");
+        $this->currentIndex++;
     }
 
-    public function setFileName($fileName = 'salarySheet') {
-        $this->fileName = $fileName;
+     private function writeRow($month, $salaryPaidOn, $commissionPaidOn) {
+        $this
+            ->spreadsheet
+            ->getActiveSheet()
+            ->setCellValue("A$this->currentIndex", $month)
+            ->setCellValue("B$this->currentIndex", $salaryPaidOn)
+            ->setCellValue("C$this->currentIndex", $commissionPaidOn);
+        $this->currentIndex++;
     }
 
-    public function getFileName() {
-        return $this->fileName;
-    }
-
-    public function setYear($year= null) {
-        if (!$year) {
-            $year = date('Y');
+    private function resizeColumns() {
+        $columns = ['A', 'B', 'C'];
+        foreach ($columns as $column) {
+            $this->spreadsheet
+                ->getActiveSheet()
+                ->getColumnDimension($column)
+                ->setWidth(50);
         }
-        $this->year = $year;
     }
-    public function getYear() {
-        return $this->year;
+
+    private function saveReport() {
+        $this->reportPath = "./reports/report_" . time() . ".xlsx";
+        $writer = $this->excelBundle->createWriter($this->spreadsheet, 'Xlsx');
+        $writer->save($this->reportPath);
     }
-    public function setMonth($month = null) {
-        if (!$month) {
-            $month = date('m');
-        }
-        $this->month = $month;
-    }
-    public function generateSheet($fileName = 'salarySheet', $year = null, $month= null) {
-        if((!$month) || ($month > 12 ) || ($month < 1))  {
-            $month = date('m');
-        }
 
-        if(!$year){
-            $year = date('Y');
-        }
-        echo ' Year '.$year.PHP_EOL;
-        echo ' month '.$month.PHP_EOL;
-
-        $this->fileName = $fileName;
-
-        if(!$this->fileName || $this->fileName ==='') {
-            $this->fileName = 'salarySheet';
-        }
-
-        $fileNameWithExte = $this->fileName.'.csv';
+    public function generateSheet($csv, $xls, $wholeYear, $fromWhichMonth, $whichYear ) {
+        $beginMonth = ($wholeYear)?intval('01'):(($fromWhichMonth)?intval($fromWhichMonth):intval(date('m' )));
+        $ofYear = $whichYear?intval($whichYear):intval(date('Y'));
         
-        $fp = fopen('./reports/'.$fileNameWithExte, "w");
-        fputcsv(
-            $fp, // The file pointer
-            array('month','Payment of salary', 'Payment of bonus'), // The fields
-            ';' // The delimiter
-        );
+        $fp = fopen('./reports/report'. time() .'.csv', "w");
+    
+        $results = [];
+        if($csv || (!$xls)) {
+            $currentMonth = $beginMonth;
+            while ($currentMonth <= 12) {
+                if($currentMonth ) {
+                    $month =$currentMonth;
+                    $year = $ofYear;
+                    if($month && $year && ($month >= 1) && ($month <= 12) ) {
+                        $results= array(
+                            date('M', strtotime($year.'-'.$month.'-1')),
+                            $this->salaryHandlerService->checkLastDayOfMonth(date('Y-m-d', strtotime($year.'-'.$month.'-01'))),
+                            $this->salaryHandlerService->getfifteenthDayOfMonth($year, $month)
+                        );
+                        fputcsv(
+                            $fp, 
+                            [
+                                ('Month: '. $results[0]),  
+                                ('Payment date of base salary '.date('\T\h\e d\t\h \o\f M Y',strtotime($results[1]))), 
+                                ('Payment date of bonus: '.date('\T\h\e d\t\h \o\f M Y',strtotime($results[2])))
+                            ],
+                            ';' 
+                        );  
+                    }
+                }
+                $currentMonth += 1;
+            }
+        }
 
-        $results= array(
-            date('M', strtotime($this->year.'-'.$this->month.'-1')),
-            $this->salaryHandlerService->checkLastDayOfMonth(date('Y-m-d', strtotime($this->year.'-'.$this->month.'-01'))),
-            $this->salaryHandlerService->getfifteenthDayOfMonth($year, $month)
-        );
-        
-        fputcsv(
-            $fp, 
-            $results, // The data
-            ';' 
-        );      
-        
-
+        if($xls) {
+            $this->writeHeader();
+            $currentMonth = $beginMonth;
+            while ($currentMonth <= 12) {
+                if($currentMonth ) {
+                    $month =$currentMonth;
+                    $year = $ofYear;
+                    if($month && $year && ($month >= 1) && ($month <= 12) ) {
+                        $results= array(
+                            date('M', strtotime($year.'-'.$month.'-1')),
+                            $this->salaryHandlerService->checkLastDayOfMonth(date('Y-m-d', strtotime($year.'-'.$month.'-01'))),
+                            $this->salaryHandlerService->getfifteenthDayOfMonth($year, $month)
+                        );
+                        $this->writeRow($results[0], $results[1],$results[2]);
+                    }
+                }
+                $currentMonth += 1;
+            }
+            $this->resizeColumns();
+            $this->saveReport();
+        }
     }
 }
